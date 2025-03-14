697732b04e9d4d671921ebe4fc0b1cee314bed93
LeStolz <vonamdang03@gmail.com>
2025-03-14 11:10:38 +0700

    More visual changes

--- Assets/Scripts/Multiplayer/Player/SerTracker/HitmarkerVisuals.cs
+++ Assets/Scripts/Multiplayer/Player/SerTracker/HitmarkerVisuals.cs
@@ -5,14 +5,24 @@ namespace Multiplayer
 	class HitMarkerVisuals : MonoBehaviour
 	{
 		[SerializeField]
-		GameObject parent;
-		const float ROTATE_SPEED = 20f;
+		GameObject child;
+		const float ROTATE_SPEED = 30f;
 		float currentRotation = 0;
 
 		void Update()
 		{
+			if (transform.position.y <= -5)
+			{
+				child.SetActive(false);
+				return;
+			}
+			else
+			{
+				child.SetActive(true);
+			}
+
 			currentRotation = (currentRotation + ROTATE_SPEED * Time.deltaTime) % 360;
-			transform.localRotation = Quaternion.Euler(0, 0, currentRotation);
+			child.transform.localRotation = Quaternion.Euler(0, 0, currentRotation);
 		}
 	}
 }
