0352bd2e8bb2b61740008ceda18ba5a2a65f071d
LeStolz <vonamdang03@gmail.com>
2025-03-20 15:44:17 +0700

    Fix stuff

--- Assets/MarkerDetection/Scripts/MarkerTracking.cs
+++ Assets/MarkerDetection/Scripts/MarkerTracking.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MarkerTracking : MonoBehaviour
 {
-    public static float MARKER_LENGTH_IN_METERS_REAL = .43f;
+    public static float MARKER_LENGTH_IN_METERS_REAL = .433f;
     static float MARKER_ERROR = 1.45f / 1.9f;
 
     public static float MARKER_LENGTH_IN_METERS => MARKER_LENGTH_IN_METERS_REAL * MARKER_ERROR;
