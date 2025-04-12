be03832f555f039cf9bc69c69b738d5ea623130c
LeStolz <vonamdang03@gmail.com>
2025-04-12 16:17:34 +0700

    Fix spawning

--- Assets/Scripts/Board/Tile.cs
+++ Assets/Scripts/Board/Tile.cs
@@ -6,6 +6,7 @@ public class Tile : NetworkBehaviour
     [Rpc(SendTo.Everyone)]
     public void SetupRpc(Vector3 pos, Vector3 rot, string tileID, bool freeze)
     {
+        transform.SetPositionAndRotation(pos, Quaternion.Euler(rot));
         gameObject.GetComponent<Rigidbody>().constraints = freeze ? RigidbodyConstraints.FreezeAll : RigidbodyConstraints.None;
         gameObject.name = tileID;
     }
